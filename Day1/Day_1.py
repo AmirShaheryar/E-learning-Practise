2320f3c208451fbf2d00fe72957727f051302f9c
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-08-02 21:20:44 +0500

    Day 1 Image Extraction

--- Day1/Day_1.py
+++ Day1/Day_1.py
@@ -16,4 +16,25 @@ for page_number,page in enumerate(docs,start=1):
                     print(f"Page {page_number}: Text: {text}, Font: {font}, Size: {size}")
 
 
+#########################Image Extraction from PDF using PyMuPDF (fitz)#########################
 
+
+import fitz
+import os
+
+doc=fitz.open("Img.pdf")
+os.makedirs("extracted_images",exist_ok=True)
+
+for page_number,page in enumerate(doc,start=1):
+    image_list=page.get_images(full=True)
+    for image_index,image in enumerate(image_list,start=1):
+
+        xref=image[0]
+        base_image=doc.extract_image(xref)
+        image_bytes=base_image["image"]
+        image_ext=base_image["ext"]
+
+        file_path=os.path.join("extracted_images",f"page_{page_number}_image_{image_index}.{image_ext}")
+        with open(file_path,"wb") as image_file:
+            image_file.write(image_bytes)
+        print(f"Saved image {image_index} from page {page_number} as {file_path}")
